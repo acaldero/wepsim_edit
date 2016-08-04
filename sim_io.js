3df9466ddc0ee9cb4df8334074516b90fa6dd67c
Alejandro Calderon <acaldero.uc3m@gmail.com>
2016-08-04 11:40:15 +0200

    First commit of the WepSIM hardware editor

---
+++ sim_io.js
@@ -0,0 +1,26 @@
+
+        //
+        // (c) Javier Prieto, F'elix Garc'ia-Carballeira, Alejandro Calder'on
+        //
+        // IO
+        //
+
+
+        /*
+         *  States
+         */
+
+        sim_states["INT"]            = { name: "INT",            nbits: "1", value: 0, default_value: 0, draw_data: [] };
+        sim_states["IORDY"]          = { name: "IORDY",          nbits: "1", value: 0, default_value: 0, draw_data: [] };
+
+
+        /*
+         *  Signals
+         */
+
+
+        /*
+         *  Syntax of behaviors
+         */
+
+
